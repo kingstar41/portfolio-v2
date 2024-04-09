f7bf9b121fbcc0bea4a40c1d11f5585063df739e
kingstar41 <kingstar9941@proton.me>
2024-04-09 19:50:34 +0100

    Chore: Add Blog
    - Add basic functionality for MDX
    
    Also:
    - Minor styling improvement for project card

---
+++ next.config.js
@@ -0,0 +1,19 @@
+// Import the withMDX function using ES module syntax
+const withMDX = require("@next/mdx")();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.simpleicons.org",
+        port: "",
+      },
+    ],
+  },
+};
+
+module.exports = withMDX(nextConfig);
